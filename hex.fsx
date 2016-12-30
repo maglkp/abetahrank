e7a7b0c7789b775b1c9374b2392b7ca69b6210f9
Lukasz Koziol <maglkp@gmail.com>
2016-12-30 17:51:42 +0100

    initial hex impl

--- hex.fsx
+++ hex.fsx
@@ -5,56 +5,57 @@ let R = 'R'
 let B = 'B'
 let E = '-'
 let VISITED = 'X'
+let NO_MOVE = (-1, -1)
 
-// read input grid, player and board size
-let (grid: string list, player: char, N: int) = 
+// read input board, player and board size
+let (board: string list, player: char, N: int) = 
     let player = Console.ReadLine().[0]
     let size = Int32.Parse(Console.ReadLine())    
     let board = List.init size (fun _ -> Console.ReadLine())    
     board, player, size
 
-let printGridList grid =
-    grid |> List.iter (fun line -> printfn "%s" line)
+let printBoardList board =
+    board |> List.iter (fun line -> printfn "%s" line)
     
-let insert (grid:string list) (element:char) (r:int, c:int) =
+let insert (board:string list) (element:char) (r:int, c:int) =
     let insertInStr (line:string) char pos =
         line.Substring(0, pos) + char.ToString() + line.Substring(pos + 1) 
-    let updatedLine = insertInStr grid.[r] element c
-    grid.[0..r-1] @ (updatedLine :: grid.[r+1..N-1])
+    let updatedLine = insertInStr board.[r] element c
+    board.[0..r-1] @ (updatedLine :: board.[r+1..N-1])
 
-let neighbours (grid:string list) r c cellValue = 
+let neighbours (board:string list) r c cellValue = 
     let rMin, rMax = max (r-1) 0, min (r+1) (N-1)
     let cMin, cMax = max (c-1) 0, min (c+1) (N-1)
     seq {
         for i = rMin to rMax do
             for j = cMin to cMax do
                 // exclude lower right and upper left corner
-                if (i, j) <> ((r-1), (c-1)) && (i, j) <> ((r+1), (c+1)) && grid.[i].[j] = cellValue then
+                if (i, j) <> ((r-1), (c-1)) && (i, j) <> ((r+1), (c+1)) && board.[i].[j] = cellValue then
                     yield (i, j)
     }
 
-let rec visit (grid:string list) (r, c) isGoal color =
-    if isGoal r c grid then
+let rec visit (board:string list) (r, c) isGoal color =
+    if isGoal r c board then
         true
     else
-        let updatedGrid = insert grid VISITED (r, c)
-        Seq.exists (fun nb -> (visit updatedGrid nb isGoal color)) (neighbours updatedGrid r c color)
+        let updatedBoard = insert board VISITED (r, c)
+        Seq.exists (fun nb -> (visit updatedBoard nb isGoal color)) (neighbours updatedBoard r c color)
         
-let isWonByBlue (grid:string list) =            
-    let isGoal r c (grid:string list) = grid.[r].[c] = B && c = N-1
-    [0..N-1] |> List.exists (fun r -> grid.[r].[0] = B && (visit grid (r, 0) isGoal B))
+let isWonByBlue (board:string list) =
+    let isGoal r c (board:string list) = board.[r].[c] = B && c = N-1
+    [0..N-1] |> List.exists (fun r -> board.[r].[0] = B && (visit board (r, 0) isGoal B))
 
-let isWonByRed (grid:string list) =            
-    let isGoal r c (grid:string list) = grid.[r].[c] = R && r = N-1
-    [0..N-1] |> List.exists (fun c -> grid.[0].[c] = R && (visit grid (0, c) isGoal R))
-    
+let isWonByRed (board:string list) =
+    let isGoal r c (board:string list) = board.[r].[c] = R && r = N-1
+    [0..N-1] |> List.exists (fun c -> board.[0].[c] = R && (visit board (0, c) isGoal R))
 
-//let rec fa() = 
-    //let uu = fb()
-    //3
-//and fb() =
-    //let tt = fa()
-    //8
+let getMoves (board:string list) =    
+    seq {
+        for i = 0 to N - 1 do
+            for j i = 0 to N - 1 do
+                if board.[i].[j] = E then
+                    yield (i, j)
+    } |> List.ofSeq
 
 let rec maxValue board a b =
     let rec bestAction moves bestMove bestv a b = 
@@ -62,11 +63,13 @@ let rec maxValue board a b =
         | m::ms ->                 
                 let minValueForNode = minValue (insert board B m) a b
                 let updatedV, updatedMove = 
+                    // update the maximal value and corresponding move
                     if minValueForNode >= bestv then
                         minValueForNode, m
                     else
                         bestv, bestMove
                 if updatedV >= b then 
+                    printfn "trimmed max"
                     updatedV, updatedMove 
                 else 
                    bestAction ms updatedMove updatedV (max a updatedV) b
@@ -74,20 +77,44 @@ let rec maxValue board a b =
 
     let blueWin = isWonByBlue board
     let redWin = isWonByRed board
-    if blueWin then 1, (0, 0)
-    else if redWin then -1, (0, 0)
+    if blueWin then 1, NO_MOVE
+    else if redWin then -1, NO_MOVE
     else
-        let v = Int32.MinValue        
-        v, (0,0)
+        let moves = (getMoves board)
+        bestAction moves (List.head moves) Int32.MinValue a b
 
 and minValue board a b =
-    let tt = maxValue board a b
-    0
+    let rec bestAction moves bestMove bestv a b =
+        match moves with
+        | m::ms ->                 
+                let maxValueForNode = fst(maxValue (insert board R m) a b)
+                let updatedV, updatedMove = 
+                    // update the minimal value and corresponding move
+                    if maxValueForNode <= bestv then
+                        maxValueForNode, m
+                    else
+                        bestv, bestMove
+                if updatedV <= a then 
+                    printfn "trimmed min"
+                    updatedV 
+                else 
+                   bestAction ms updatedMove updatedV a (min b updatedV)
+        | [] -> bestv
+
+    let blueWin = isWonByBlue board
+    let redWin = isWonByRed board
+    if blueWin then 1
+    else if redWin then -1
+    else
+        let moves = (getMoves board)
+        bestAction moves (List.head moves) Int32.MaxValue a b
+
+let absearch board = 
+    maxValue board Int32.MinValue Int32.MaxValue 
 
 
-//printGridList (insert grid '@' 10 10)
-//printfn "%A" (isWonByRed grid)
-//printfn "%A" (isWonByBlue grid)
-
+//printBoardList (insert board '@' 10 10)
+//printfn "%A" (isWonByRed board)
+printfn "%A" (absearch board)
 printfn "\ndone..." 
 
